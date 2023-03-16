0c2c8ff731849ea6e55e1f40bdce452810b225bd
ElRo <elia.ro91@gmail.com>
2023-03-16 23:26:24 +0100

    chore():  just need to convert from base64 to text

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,43 @@
 pub mod key_gen{
     extern crate openssl;
 
+    use std::str;
+    use std::string::String;
+    use std::vec::Vec;
+    use std::convert::TryInto;
+    use std::io::Write;
+    use openssl::error::ErrorStack;
     use openssl::rsa::{Rsa, Padding};
     use openssl::symm::Cipher;
     
 
-    pub fn pub_key() -> String {
+    pub fn initialize_key_pair() -> (String, String) {
     
-        let passphrase = "rust_by_example";
+        
 
         let rsa = Rsa::generate(1024).unwrap();
-        let private_key: Vec<u8> = rsa.private_key_to_pem_passphrase(Cipher::aes_128_cbc(), passphrase.as_bytes()).unwrap();
+        let private_key: Vec<u8> = rsa.private_key_to_pem().unwrap();
         let public_key: Vec<u8> = rsa.public_key_to_pem().unwrap();
     
-        println!("Private key: {}", String::from_utf8(private_key).unwrap());
-        String::from_utf8(public_key).unwrap()
+        (String::from_utf8(private_key).unwrap(), String::from_utf8(public_key).unwrap())
 
+    }
+
+    pub fn encrypt_with_rsa_public_key(pub_key: String, plaintext: String) -> Result<String, ErrorStack> {
+        let rsa = Rsa::public_key_from_pem(pub_key.as_bytes())?;
+        let mut ciphertext = vec![0; rsa.size() as usize];
+        let len = rsa.public_encrypt(plaintext.as_bytes(), &mut ciphertext, Padding::PKCS1)?;
+        ciphertext.truncate(len);
+        Ok(base64::encode(&ciphertext))
+    }
+
+    pub fn decrypt_with_rsa_private_key(pvt_key: String, base64text: String) -> Result<String, ErrorStack> {
+        let rsa = Rsa::private_key_from_pem(pvt_key.as_bytes())?;
+        let plaintext = base64::decode(&base64text).unwrap();
+        let mut ciphertext = vec![0; rsa.size() as usize];
+        let len = rsa.private_decrypt(&plaintext, &mut ciphertext, Padding::PKCS1)?;
+        ciphertext.truncate(len);
+        Ok(base64::encode(&ciphertext))
     }
 
 }
--- src/main.rs
+++ src/main.rs
@@ -10,29 +10,48 @@ fn main() {
         println!("Failed to initialize GTK.");
         return;
     }
+    
+    let key_pair: (String, String) = key_gen::initialize_key_pair();
 
     let window = Window::new(WindowType::Toplevel);
     window.set_title("Hello, World!");
     window.set_default_size(350, 70);
 
     let button = Button::new();
-    button.set_label("Click me!");
+    button.set_label("Encrypt");
 
-    let pub_key = key_gen::pub_key().to_owned();
+    // Get Public Key
+    let private_key = key_pair.0;
+    let pub_key = key_pair.1;
+
+    println!("{}", private_key);
 
     let entry = Rc::new(RefCell::new(Entry::new()));
     entry.borrow().set_text(&pub_key);
 
     let entry2 = Rc::new(RefCell::new(Entry::new()));
-    entry2.borrow().set_text("Insert text here");
+    entry2.borrow().set_text("Insert text to encrypt");
+
+    let (tx, rx) = std::sync::mpsc::channel();
 
     let entry_clone = entry.clone();
     let entry_clone2 = entry2.clone();
+
     button.connect_clicked(move |_| {
-        let text = entry_clone.borrow().get_text().to_string();
-        let text2 = entry_clone2.borrow().get_text().to_string();
-        println!("Button clicked with text: {}", text);
+        let pub_key_string = entry_clone.borrow().get_text().to_string();
+        let text_to_encypt = entry_clone2.borrow().get_text().to_string();
+        let text_encrypted = key_gen::encrypt_with_rsa_public_key(pub_key_string, text_to_encypt);
+        println!("Encrypted text:\n{}", text_encrypted.clone().unwrap());
+        tx.send(text_encrypted.unwrap()).unwrap();
     });
+    std::thread::spawn(move || {
+        let text = rx.recv().unwrap();
+        let text_decrypted = key_gen::decrypt_with_rsa_private_key(private_key.clone(), text.clone());
+        println!("Decrypted text:\n{}", text_decrypted.clone().unwrap());
+    });
+
+   
+    
 
     let vbox = gtk::Box::new(gtk::Orientation::Vertical, 0);
     vbox.add(&*entry.borrow());
